b75ae5b07ac565a9c3c85c1b8d99bfcdcb33ede1
André Lira <andrelira50@gmail.com>
2025-12-08 22:47:49 -0300

    fix: tentando ajeitar trocando para ipv4

--- Backend/config/runtime.exs
+++ Backend/config/runtime.exs
@@ -49,9 +49,10 @@ if config_env() == :prod do
   config :easy_dash, EasyDashWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: {0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base,
-    check_origin: false
+    check_origin: false,
+    server: true
 end
